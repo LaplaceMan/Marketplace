cc41a53a604e3c523f294c43d7e3285d9069b658
LaplaceMan <74650892+LaplaceMan@users.noreply.github.com>
2022-06-21 14:57:10 +0800

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,19 +1,13 @@
 import React, { useContext } from 'react'
 import Welcome from './Welcome'
 import NftSection from './Section'
-import { MarketplaceContext } from '../context/MarketplaceContext';
 
 export default function Home() {
-
-  const {allCollections, fetchNFTs} = useContext(MarketplaceContext);
 
   return (
     <div>
         <Welcome />
-        <NftSection 
-            allCollections = {allCollections}
-            allnfts={fetchNFTs}
-        />
+        <NftSection />
     </div>
   )
 }
