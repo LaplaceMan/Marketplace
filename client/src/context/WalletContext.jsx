a52fe528abd6617811f882acf8d298d58ed45fbe
LaplaceMan <74650892+LaplaceMan@users.noreply.github.com>
2022-06-21 14:56:06 +0800

    Update WalletContext.jsx

--- client/src/context/WalletContext.jsx
+++ client/src/context/WalletContext.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from 'ethers';
 
-import { contractABI, contractAddress, networks } from '../utils/constants';
+import { networks } from '../utils/constants';
 
 export const WalletContext = React.createContext();
 
